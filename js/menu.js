071607fdd7426ef8189915974e9de336ed52eb19
gopinathmukherjee272006-del <gopinathmukherjee272006@gmail.com>
2025-12-19 11:10:08 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,48 +1,35 @@
 
-fetch("header.html")
-.then(res => res.text())
-.then(data => {
-	document.getElementById("header").innerHTML = data;
-    let currentPage = window.location.pathname.split("/").pop();
-    if ( !currentPage || currentPage == "#" ) {
-		currentPage = "index.html";
-    }
-    // menu me anchor tak pahuchne ka tareeka
-    let links = document.querySelectorAll(".menu .menubar ul li a");
-        
-	// hr anchor tag ko process karenge
-    links.forEach(link => {
-		//pehle  anchor tag ka href ki value save karenge
-    	let linkPage = link.getAttribute("href");
-		
-        if(!linkPage) return;
-  
-        if (linkPage == currentPage) {
-      		link.closest("li").classList.add("underline_current");
-    	}
 
-		if (window.innerWidth <= 576) {
-			const menu = document.querySelector(".menu");
-			const menuBar = document.querySelector(".menu .menubar ul");
-			const mobileBar = document.querySelector(".menu .mobilebar");
 
-			if(mobileBar && !mobileBar.querySelector(".hamburger")) {
-				const dv = document.createElement("div");
-				dv.className = "hamburger";
-				dv.setAttribute("aria-label", "Toggle menu");
-				dv.setAttribute("tabindex","0");
-				dv.innerHTML= '<span></span>';
-				mobileBar.appendChild(dv);
+<html>
+  <head>
+    <title>Project</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  </head>
+  <body>
+    <div id="nav"></div>
+    <script>
+      fetch("navbar.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("nav").innerHTML = data;
 
-				dv.addEventListener("click", function(e) {
-					e.stopPropagation();
-					menu.classList.toggle("mobile-open");
-				});
-			}
-		}
-	});
-
-}) // end then
-.catch(err => {
-    console.error("Header load failed:", err);
-});
+        let currentpage = window.location.pathname.split("/").pop();
+        let links = document.querySelectorAll(".menu .menubar ul li a");
+        if(!currentpage  || currentpage == "#"){
+          currentpage ="index.html";
+        }
+        links.forEach(link=> {
+          console.log("current page:",currentpage);
+          let linkpage = link.getAttribute("href");
+          if (linkpage == currentpage){
+          link.closest("li").classList.add("underline_current");
+          }
+      });
+        });
+     
+    </script>
+  </body>
+</html>
+     
